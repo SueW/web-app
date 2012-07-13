f60925610d743dd11b26f0b04fa6557e19a26c64
SueW <susanwoodford17@gmail.com>
2012-07-13 11:02:53 -0400

    Added classes to header.

--- chiptruck-app/index.php
+++ chiptruck-app/index.php
@@ -12,70 +12,71 @@ $chiptruck = 'main-page';
 		<title>Ottawa Chip Truck Finder</title>
 		<link href="css/general.css" rel="stylesheet">
 	</head>
-
-<body>
-
-<div class="ottawa"><h1><strong>Ottawa</strong></h1></div>
-
-<div class="logo"><img src="images/chiptruck.png" alt="Chip Truck"></div>
-
-<div class ="finder"><h1><strong>Finder</strong></h1></div>
-
-<div class="container">
-<h2 div class="fryfinder">French Fry Finder for Folks on the Fly</h2>
-
-<h3 div class="location">Choose a location:</h3>
-
-<nav>
-	<ul>
-		<li<?php if ($chiptruck !='downtown' && $chiptruck != 'central' && $chiptruck != 'south' && $chiptruck != 'east' && $chiptruck != 'west' && $chiptruck != 'all') {?> class="current"<?php } ?>><a href="?chiptruck=main-page">Map</a></li>
-		<li<?php if ($chiptruck == 'downtown') { ?> class="current"<?php } ?>><a href="?chiptruck=downtown">Downtown</a></li>
-		<li<?php if ($chiptruck == 'central') { ?> class="current"<?php } ?>><a href="?chiptruck=central">Central</a></li>
-		<li<?php if ($chiptruck == 'south') { ?> class="current"<?php } ?>><a href="?chiptruck=south">South</a></li>
-		<li<?php if ($chiptruck == 'east') { ?> class="current"<?php } ?>><a href="?chiptruck=east">East</a></li>
-		<li<?php if ($chiptruck == 'west') { ?> class="current"<?php } ?>><a href="?chiptruck=west">West</a></li>
-		<li<?php if ($chiptruck == 'all') { ?> class="current"<?php } ?>><a href="?chiptruck=all">All</a></li>
-	</ul>
-</nav>
-
-<article>
-	<?php
-		switch ($chiptruck) {
-			case 'downtown' :
-				include 'includes/downtown.php';
-			break;
-			
-			case 'central' :
-				include 'includes/central.php';
-			break;
-			
-			case 'south' :
-				include 'includes/south.php';
-			break;
-			
-			case 'east' :
-				include 'includes/east.php';
-			break;
-			
-			case 'west' :
-				include 'includes/west.php';
-			break;
-			
-			case 'all' :
-				include 'includes/all.php';
-			break;
-			
-			default:
-				include 'includes/main-page.php';
-			break;
-			
-		}
 	
-	?>
-
-</article>
-
-</div>
-
-</body>
+		<body>
+		
+			<div class="ottawa"><h1><strong>Ottawa</strong></h1></div>
+			
+			<div class="logo"><img src="images/chiptruck.png" alt="Chip Truck"></div>
+		
+			<div class ="finder"><h1><strong>Finder</strong></h1></div>
+			
+			<div class="container">
+			
+			<h2 div class="fryfinder">French Fry Finder for Folks on the Fly</h2>
+			
+			<h3 div class="location">Choose a location:</h3>
+			
+			<nav>
+				<ul>
+					<li<?php if ($chiptruck !='downtown' && $chiptruck != 'central' && $chiptruck != 'south' && $chiptruck != 'east' && $chiptruck != 'west' && $chiptruck != 'all') {?> class="current"<?php } ?>><a href="?chiptruck=main-page">Map</a></li>
+					<li<?php if ($chiptruck == 'downtown') { ?> class="current"<?php } ?>><a href="?chiptruck=downtown">Downtown</a></li>
+					<li<?php if ($chiptruck == 'central') { ?> class="current"<?php } ?>><a href="?chiptruck=central">Central</a></li>
+					<li<?php if ($chiptruck == 'south') { ?> class="current"<?php } ?>><a href="?chiptruck=south">South</a></li>
+					<li<?php if ($chiptruck == 'east') { ?> class="current"<?php } ?>><a href="?chiptruck=east">East</a></li>
+					<li<?php if ($chiptruck == 'west') { ?> class="current"<?php } ?>><a href="?chiptruck=west">West</a></li>
+					<li<?php if ($chiptruck == 'all') { ?> class="current"<?php } ?>><a href="?chiptruck=all">All</a></li>
+				</ul>
+			</nav>
+			
+			<article>
+				<?php
+					switch ($chiptruck) {
+						case 'downtown' :
+							include 'includes/downtown.php';
+						break;
+						
+						case 'central' :
+							include 'includes/central.php';
+						break;
+						
+						case 'south' :
+							include 'includes/south.php';
+						break;
+						
+						case 'east' :
+							include 'includes/east.php';
+						break;
+						
+						case 'west' :
+							include 'includes/west.php';
+						break;
+						
+						case 'all' :
+							include 'includes/all.php';
+						break;
+						
+						default:
+							include 'includes/main-page.php';
+						break;
+						
+					}
+				
+				?>
+			
+			</article>
+			
+		</div>
+	
+	</body>
 </html>
