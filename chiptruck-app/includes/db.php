deca1d912e7591e9e394efc75836e98fcc61712f
SueW <susanwoodford17@gmail.com>
2012-08-08 13:28:04 -0400

    made changes to the db file for php cloud

--- chiptruck-app/includes/db.php
+++ chiptruck-app/includes/db.php
@@ -3,9 +3,12 @@
 //Opens a connection to the MySQL database
 // Shared between all the PHP files in our application
 
-$user = getenv('DB_USER'); //The MySQL username
-$pass = getenv('DB_PASS'); // The MySQL password
-$data_source = getenv('DATA_SOURCE');
+$user = getenv('MYSQL_USERNAME'); //The MySQL username
+$pass = getenv('MYSQL_PASSWORD'); // The MySQL password
+$host = getenv('MYSQL_DB_HOST'); 
+$dbname = getenv('MYSQL_DB_NAME');
+
+$data_source = sprintf('mysql:host=%s;dbname=%s', $host, $dbname);
 
 // PDO: PHP Data Objects
 // Allows us to connect to many different kinds of databases
